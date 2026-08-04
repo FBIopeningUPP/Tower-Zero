55ed5836858c364f53a4010e1157bc8491393265
Milind Singh <milindsingh486@gmail.com>
2026-08-04 23:40:11 +0530

    more shit
    more shit

--- tower-zero/systems/spawners/EnemySpawner.gd
+++ tower-zero/systems/spawners/EnemySpawner.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
-@export var enemy_scene: PackedScene = preload("res://entities/enemies/Enemy.tscn")
 @export var ground_enemy: PackedScene = preload("res://entities/enemies/Enemy.tscn")
 @export var drone_enemy: PackedScene = preload("res://entities/enemies/Drone.tscn")
-@export var spawn_interval: float = 3
+@export var starting_spawn_interval: float = 3
+@export var minimum_spawn_interval: float = 0.5
+
+var current_interval: float
+var timer: Timer
 
 func _ready() -> void:
-	var timer = Timer.new()
-	timer.wait_time = spawn_interval
+	current_interval = starting_spawn_interval
+	
+	timer = Timer.new()
+	timer.wait_time = current_interval
 	timer.autostart = true
 	timer.timeout.connect(_on_spawn_timer_timeout)
 	add_child(timer)
+	
+	EventBus.enemy_died.connect(_on_enemy_died)
+	
+func _on_enemy_died(xp: int) -> void:
+	current_interval = max(current_interval * 0.98, minimum_spawn_interval)
+	timer.wait_time = current_interval
 
 func _on_spawn_timer_timeout() -> void:
 	var scene_to_spawn = ground_enemy
