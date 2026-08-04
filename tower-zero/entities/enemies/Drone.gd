50864e9f45fdb4d32db352e92e34b9bc6ba96e59
Milind Singh <milindsingh486@gmail.com>
2026-08-04 23:28:30 +0530

    pres r to restart i hat ecommiting

--- tower-zero/entities/enemies/Drone.gd
+++ tower-zero/entities/enemies/Drone.gd
@@ -1,34 +1,33 @@
 extends CharacterBody2D
-class_name Enemy
+class_name Drone
 
-@export var speed: float = 100
+@export var speed: float = 120.0
 @export var damage: int = 10
 
 var death_particles = preload("res://scenes/effects/DeathParticles.tscn")
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction: int = -1
-
 @onready var health_component: HealthComponent = $HealthComponent
+var target: Node2D = null
 
 func _ready() -> void:
 	if health_component:
 		health_component.health_depleted.connect(_on_death)
 		health_component.health_changed.connect(func(c, m): EventBus.enemy_damaged.emit())
 		
+	# Find the player!
+	var players = get_tree().get_nodes_in_group("Player")
+	if players.size() > 0:
+		target = players[0]
+		
 func _on_death() -> void:
 	var explosion = death_particles.instantiate()
 	get_parent().add_child(explosion)
 	explosion.global_position = self.global_position
-	
 	queue_free()
-	EventBus.enemy_died.emit(10)
 	
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	velocity.x = direction * speed
-	
-	if is_on_wall():
-		direction *= -1
-	
+	# Ignore gravity! Just fly straight at the target.
+	if target:
+		var direction = global_position.direction_to(target.global_position)
+		velocity = direction * speed
+		
 	move_and_slide()
