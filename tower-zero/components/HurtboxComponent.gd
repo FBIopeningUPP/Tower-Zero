cbe21fc63bf3e585441cf25a9c80248be56a278b
Milind Singh <milindsingh486@gmail.com>
2026-08-04 18:33:47 +0530

    enemy
    enemy

---
+++ tower-zero/components/HurtboxComponent.gd
@@ -0,0 +1,11 @@
+extends Area2D
+class_name HurtboxComponent
+
+@export var health_component: HealthComponent
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is HitboxComponent and health_component != null:
+		health_component.take_damage(area.damage)
