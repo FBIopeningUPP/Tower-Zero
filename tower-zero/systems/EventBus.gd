77e9f7412ae4ee6c2729f9e980f691524a6498ed
Milind Singh <milindsingh486@gmail.com>
2026-08-06 01:13:35 +0530

    used ai for bug fixing

--- tower-zero/systems/EventBus.gd
+++ tower-zero/systems/EventBus.gd
@@ -1,20 +1,19 @@
 extends Node
 
-
 signal player_health_changed(current, max_hp)
 signal player_died
 signal enemy_died(xp)
 signal floor_cleared(floor_number: int)
-signal hit_landed 
+signal floor_started(floor_number: int)
+signal hit_landed
 signal next_floor_requested
+signal enemy_damaged
+signal player_energy_changed(current, max_energy)
 
 func _ready() -> void:
-	hit_landed.connect(_on_hit_landed)
+	pass
 
 func _on_hit_landed() -> void:
 	Engine.time_scale = 0.0
-	await get_tree().create_timer(0.05, true, false, true).timeout # ignore time scale
+	await get_tree().create_timer(0.05).timeout
 	Engine.time_scale = 1.0
-
-signal enemy_damaged
-signal player_energy_changed(current, max_energy)
