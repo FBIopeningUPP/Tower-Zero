650a1cad73a632cdc35454839c672a3f60dac4a1
Milind Singh <milindsingh486@gmail.com>
2026-08-08 00:33:03 +0530

    lotta change

--- tower-zero/components/HitboxComponent.gd
+++ tower-zero/components/HitboxComponent.gd
@@ -5,3 +5,9 @@ enum Element {NONE, FIRE, POISON, ELECTRIC}
 
 @export var damage: int = 10
 @export var element_type: Element = Element.NONE
+
+func _ready() -> void:
+	# Hitboxes must exist on Layer 3 (value 4) so Hurtboxes can detect them!
+	collision_layer |= 4
+	collision_mask = 0 # Hitboxes don't scan, Hurtboxes do the scanning
+
